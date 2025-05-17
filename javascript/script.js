c6f80449f42498998f26b716ee6228ce2c92669b
paulchris7 <paulchristaimeslg3@gmail.com>
2025-05-17 15:06:45 +0100

    Remove unused skill progress bar functionality and clean up code

--- javascript/script.js
+++ javascript/script.js
@@ -24,7 +24,6 @@ var typed = new Typed(".typing-text", {
 });
 
 // Animated Statistics Counter
-
 document.addEventListener('DOMContentLoaded', () => {
   const statsSection = document.querySelector('.about-stats');
   const counters = document.querySelectorAll('.count');
@@ -84,8 +83,6 @@ buttons.forEach((button, index) => {
   });
 });
 
-
-
 // portfolio fillter 
 const buttons2 = document.querySelectorAll('.fillter-buttons button');
 buttons2.forEach((button, index) => {
@@ -130,60 +127,7 @@ var swiper = new Swiper(".mySwiper", {
     }
   });
 
-
-
-//   skill Progress bar 
-
-const first_skill = document.querySelector(".skill:first-child");
-const sk_counters = document.querySelectorAll(".counter span");
-const progress_bars = document.querySelectorAll(".skills svg circle");
-
-window.addEventListener("scroll",()=>{
-    if(!skillsPlayed)
-    skillsCounter();
-})
-
-
-function hasReached(el){
-    let topPosition = el.getBoundingClientRect().top;
-    if(window.innerHeight >= topPosition + el.offsetHeight)return true;
-    return false;
-}
-
-function updateCount(num,maxNum){
-    let currentNum = +num.innerText;
-    
-    if(currentNum < maxNum){
-        num.innerText = currentNum + 1;
-        setTimeout(()=>{
-            updateCount(num,maxNum)
-        },12)
-    }
-}
-
-
-let skillsPlayed = false;
-
-function skillsCounter(){
-    if(!hasReached(first_skill))return;
-    skillsPlayed = true;
-    sk_counters.forEach((counter,i)=>{
-        let target = +counter.dataset.target;
-        let strokeValue = 465 - 465 * (target / 100);
-
-        progress_bars[i].style.setProperty("--target",strokeValue);
-
-        setTimeout(()=>{
-            updateCount(counter,target);
-        },400)
-    });
-
-    progress_bars.forEach(p => p.style.animation = "progress 2s ease-in-out forwards");
-}
-
-
 // side progress bar 
-
 let calcScrollValue = ()=>{
     let scrollProgress = document.getElementById("progress");
     let pos = document.documentElement.scrollTop;
